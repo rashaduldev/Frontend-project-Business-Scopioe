7fc7b31a69502f501d3744ca34b44962e51e95aa
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2024-08-02 06:16:05 +0600

    Tailwind  implement

---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,11 @@
+
+
+const Homepage = () => {
+    return (
+        <div className="text-5xl">
+            home pages
+        </div>
+    );
+};
+
+export default Homepage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Homepage from './Pages/Homepage';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage/>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
